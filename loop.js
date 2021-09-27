81c1487be622f0a49ab626b361a6cec6a7126088
Elham <mireshghi@yahoo.com>
2021-09-27 22:10:02 +0000

    loop.js

---
+++ loop.js
@@ -0,0 +1,56 @@
+/*const number
+for(number=100;number<200;number++){
+    if(number/3===0) console,log("Loopy");
+    else if (number/5===0) console.log("Lighthouse" );
+    else (number/5===0 && number/3===0); console.log("LoopyLighthouse") } */
+
+/*
+//const number;
+for( var number=100 ; number <=200;number++){
+    if(number%5===0 && number%3===0) {console.log("LoopyLighthouse");}
+    else if (number%5===0){ console.log("Lighthouse" );}
+    else (number%3===0)  ;{ console.log("Loopy")}
+    console.log(numb
+
+
+
+
+
+
+
+
+ for ( var number =100 ; number <= 200 ; number++)  {
+    if(number %5 === 0 && number %3 === 0) {  console.log("LoopyLighthouse");  }
+    else if ( number %5 === 0)  {
+      console.log("Lighthouse");
+      
+    }
+    else if  (number%3 === 0 )    {
+      console.log ("Loopy");  }
+ //console.log(number);
+}
+*/
+
+
+for( var number =100 ; number <= 200 ; number++)  {
+  if(number %5 === 0 && number %3 === 0) {  console.log("LoopyLighthouse");  }
+  else if ( number %5 === 0)  {
+    console.log("Lighthouse");
+    
+  }
+  else if  (number%3 === 0 )    {
+    console.log ("Loopy");  }
+else{console.log(number);}
+}
+
+for( var number = 100 ; number <= 200 ; number++ )   {
+  if ( number %4===0 &&  number  %3  === 0 ) {  
+    console.log("LoopyLighthouse");  }
+  else if ( number %4 === 0)  {
+    console.log("Lighthouse");
+    
+  }
+  else if  ( number %3  === 0 )    {
+    console.log ("Loopy");  } 
+else { console.log(number); }
+}
